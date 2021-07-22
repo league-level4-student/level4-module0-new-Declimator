c1444281c1dbf4ddc6d73d4ec33efc61e5722019
Declimator <31232768+Declimator@users.noreply.github.com>
2021-07-22 16:59:06 -0700

    EncapsulateData, DataType IP

--- src/_01_Encapsulate_the_Data/EncapsulateTheData.java
+++ src/_01_Encapsulate_the_Data/EncapsulateTheData.java
@@ -7,7 +7,7 @@ public class EncapsulateTheData {
      * itemsReceived to 0.
      */
 
-    int itemsReceived;
+    protected int itemsReceived;
     
     public int getItemsReceived() {
     	return itemsReceived; 
@@ -19,26 +19,55 @@ public class EncapsulateTheData {
     	else
     		itemsReceived = 0;
     }
+    
     /*
      * degreesTurned must be locked between 0.0 and 360.0 inclusive. All
      * parameters outside this range should set degreesTurned to the nearest
      * bound.
      */
-
-    double degreesTurned;
-
+   
+    protected double degreesTurned;
+    
+    public double getDegreesTurned() {
+    	return degreesTurned;
+    }
+    public void setDegreesTurned(double degrees) {
+    	if(degrees < 0)
+    		degrees = 0;
+    	else if(degrees > 360)
+    		degrees = 360;
+    	degreesTurned = degrees;
+    }
     /*
      * nomenclature must not contain an empty String. An empty String parameter
      * should set nomenclature to a String with a single space.
      */
 
-    String nomenclature;
-
+    protected String nomenclature;
+    
+    public String getNomenclature() {
+    	return nomenclature;
+    }
+    
+    public void setNomenclature(String nomenclature) {
+    	if(nomenclature.isEmpty())
+    		nomenclature = " ";
+    	this.nomenclature = nomenclature;
+    }
     /*
      * memberObj must not be a String. A String parameter should set memberObj
      * to a new Object(); Hint: Use the instanceof operator.
      */
 
-    Object memberObj;
-
+    protected Object memberObj;
+    
+    public Object getMemberObj() {
+    	return memberObj;
+    }
+    public void setMemberObj(Object obj) {
+    	if(obj instanceof String) {
+    		obj = new Object();
+    	}
+    	memberObj = obj;
+    }
 }
