44a6a931668182acf78481bacdd7fc125c928eb6
Declimator <31232768+Declimator@users.noreply.github.com>
2021-07-22 16:59:39 -0700

    DataType IP

--- src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
+++ src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class DataTypeOptionPane {
 	enum DataType {
-		
+		BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, BOOLEAN, CHAR
 	}
     public static void main(String[] args) {
 
