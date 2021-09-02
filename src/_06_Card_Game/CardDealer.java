1e573f66e540727d9bc5025b58b5847a0f381212
Declimator <31232768+Declimator@users.noreply.github.com>
2021-09-02 16:57:33 -0700

    CardGame IP

--- src/_06_Card_Game/CardDealer.java
+++ src/_06_Card_Game/CardDealer.java
@@ -4,7 +4,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class CardDealer {
-    
+    public static void main(String[] args) {
+    	CardDealer dealer = new CardDealer();
+		dealer.dealCards();
+	}
     ArrayList<Card> deck;
-
+    public void dealCards() {
+    	deck = new ArrayList<Card>();
+    	for(int i = 0; i < Suit.values().length; i++) {
+    		for(int j = 0; j < Rank.values().length; j++) {
+    			deck.add(new Card(Rank.values()[j], Suit.values()[i]));
+    		}
+    	}
+    }
 }
